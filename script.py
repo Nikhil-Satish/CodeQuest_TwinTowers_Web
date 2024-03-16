585015d458cd4214bfa17c9bc3ca8f1cea0e6cda
Nikhil-Satish <satishnikhil307@gmail.com>
2024-03-16 23:07:20 +0530

    Added Script

---
+++ script.py
@@ -0,0 +1,22 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str, age:int, display_age:bool=True):
+    print(f"Hello {name}")
+    if display_age:
+        print(f"Age {age}")
+    # print(f"Age {age}")
+
+@app.command()
+def goodbye():
+    print("Goodbye")
+
+@app.command()
+def filter(filename:str, conditions:list[str]):
+    print(filename)
+    print(conditions)
+    
+if __name__ == "__main__":
+    app()
