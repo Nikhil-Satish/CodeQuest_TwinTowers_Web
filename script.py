4154c2b8e9af4ffe31b90fc7e2b7323e17bbc3ac
Nikhil-Satish <satishnikhil307@gmail.com>
2024-03-17 03:36:36 +0530

    Updates

--- script.py
+++ script.py
@@ -1,7 +1,51 @@
 import typer
 from csv_test import filtering, aggregating, joining
+import pandas as pd
+import csv
 
 app = typer.Typer()
+
+def format_delim(delim):
+    if len(delim) ==1 or len(delim) ==2:
+        if len(delim) == 2:
+            if delim[0] != "\\":
+                print("Incorrect delimiter entered")
+            else:
+                if delim[1] == 't':
+                    delim = '\t'
+                elif delim[1] == 'n':
+                    delim = '\n'
+                else:
+                    print("Invalid delimiter entered")
+    else:
+        print("Delimiter of incorrect length entered. Please enter a delimiter of length 1 or 2")
+    return delim
+
+@app.command()
+def filter(filename:str, conditions:list[str], delim:str=',', quoting:int=0):
+    df = filtering(filename, conditions)
+    print(df)
+    df = pd.DataFrame(df)
+    delim = format_delim(delim)
+
+    df.to_csv('output.csv', sep=delim, quoting=quoting)
+
+@app.command()
+def aggregate(filepath:str, column:str):
+    aggregating(filepath, column)
+
+@app.command()
+def join(column:str, paths:list[str], delim:str=',', quoting:int=0):
+    # aggregating(filepath1, column)
+    # paths = [filepath1, filepath2]
+    df = joining(paths, column)
+    if type(df) == type(0):
+        return
+    print(df)
+    df = pd.DataFrame(df)
+    delim = format_delim(delim)
+    df.to_csv('output.csv', sep=delim, quoting=quoting)
+
 
 @app.command()
 def hello(name: str, age:int, display_age:bool=True):
@@ -13,22 +57,6 @@ def hello(name: str, age:int, display_age:bool=True):
 @app.command()
 def goodbye():
     print("Goodbye")
-
-@app.command()
-def filter(filename:str, conditions:list[str]):
-    df = filtering(filename, conditions)
-    print(df)
-
-@app.command()
-def aggregate(filepath:str, column:str):
-    aggregating(filepath, column)
-
-@app.command()
-def join(column:str, paths:list[str]):
-    # aggregating(filepath1, column)
-    # paths = [filepath1, filepath2]
-    df = joining(paths, column)
-    print(df)
 
 if __name__ == "__main__":
     app()
